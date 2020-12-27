c2c699fc6370fad90b740f74ba7df0c612729fff
Pawel Jurecki <jurecki.pawel@gmail.com>
2020-12-27 21:22:09 +0100

    Add settings.js with arrays of all people, starsthips and vehicles ID

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import 'regenerator-runtime/runtime' //async/await with Parcel
 import {App} from "./app/App";
+import '../sass/style.scss';
 
 const ONE_SECOND_MILLIS = 1000;
 const SW_API_BASE_URL = process.env.SW_API_BASE_URL || "https://swapi.dev/api";
