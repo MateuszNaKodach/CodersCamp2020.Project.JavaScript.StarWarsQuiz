1052d05a7476e26d45a857c0f09d4bb64eab0671
Pawel Jurecki <jurecki.pawel@gmail.com>
2020-12-27 23:59:39 +0100

    Problem with test fetchData function

---
+++ src/utils/fetchData.test.js
@@ -0,0 +1,71 @@
+const fechMock = require('jest-fetch-mock');
+fechMock.enableMocks();
+
+const {fetchData} = require('./fetchData');
+
+it('should generate correct data for people mode', () => {
+
+	fetch.mockResponseOnce({
+		"name": "Luke Skywalker",
+		"height": "172",
+		"mass": "77",
+		"hair_color": "blond",
+		"skin_color": "fair",
+		"eye_color": "blue",
+		"birth_year": "19BBY",
+		"gender": "male",
+		"homeworld": "http://swapi.dev/api/planets/1/",
+		"films": [
+			"http://swapi.dev/api/films/1/",
+			"http://swapi.dev/api/films/2/",
+			"http://swapi.dev/api/films/3/",
+			"http://swapi.dev/api/films/6/"
+		],
+		"species": [],
+		"vehicles": [
+			"http://swapi.dev/api/vehicles/14/",
+			"http://swapi.dev/api/vehicles/30/"
+		],
+		"starships": [
+			"http://swapi.dev/api/starships/12/",
+			"http://swapi.dev/api/starships/22/"
+		],
+		"created": "2014-12-09T13:50:51.644000Z",
+		"edited": "2014-12-20T21:17:56.891000Z",
+		"url": "http://swapi.dev/api/people/1/"
+	})
+
+	const result = fetchData('people', 1).then (data => data);
+
+	expect(fetch).toHaveBeenCalledWith('https://swapi.dev/api/people/1/')
+	expect(result).toEqual({
+		"name": "Luke Skywalker",
+		"height": "172",
+		"mass": "77",
+		"hair_color": "blond",
+		"skin_color": "fair",
+		"eye_color": "blue",
+		"birth_year": "19BBY",
+		"gender": "male",
+		"homeworld": "http://swapi.dev/api/planets/1/",
+		"films": [
+			"http://swapi.dev/api/films/1/",
+			"http://swapi.dev/api/films/2/",
+			"http://swapi.dev/api/films/3/",
+			"http://swapi.dev/api/films/6/"
+		],
+		"species": [],
+		"vehicles": [
+			"http://swapi.dev/api/vehicles/14/",
+			"http://swapi.dev/api/vehicles/30/"
+		],
+		"starships": [
+			"http://swapi.dev/api/starships/12/",
+			"http://swapi.dev/api/starships/22/"
+		],
+		"created": "2014-12-09T13:50:51.644000Z",
+		"edited": "2014-12-20T21:17:56.891000Z",
+		"url": "http://swapi.dev/api/people/1/"
+	})
+	
+})
