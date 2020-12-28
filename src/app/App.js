777ea78bbb8fe4101957f7f04a711513fbba8edc
PiotrWR <pwrynio@gmail.com>
2020-12-28 22:15:08 +0100

    Init commit for menu.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,7 @@
-export const App = ({options}) => {
-}
+import { Menu } from './layouts/Menu';
 
-
+export const App = ({ options }) => {
+  const app = document.getElementById('swquiz-app');
+  console.log('Działa App!');
+  app.appendChild(Menu());
+};
---
+++ src/app/layouts/Menu.js
@@ -0,0 +1,12 @@
+export const Menu = (image) => {
+  console.log('Działa Menu!');
+  const menuDomObj = document.createElement('div');
+
+  // image = atob(image);
+
+  // imageWrapper.classList.add('swquiz-app-image');
+  // imageWrapper.style.backgroundImage = `url(${image})`;
+  // app.appendChild(imageWrapper);
+
+  return menuDomObj;
+};
