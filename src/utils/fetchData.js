cc8a8fd92150a656b857f38a2a18d6d05cc52880
Pawel Jurecki <jurecki.pawel@gmail.com>
2020-12-28 10:47:12 +0100

    Add test to fetchData function

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,7 +1,6 @@
 export function fetchData (mode,id) {
-	return fetch(`https://swapi.dev/api/${mode}/${id}/`)
+	return  fetch(`https://swapi.dev/api/${mode}/${id}/`)
 	.then(response => response.json())
 	.then(data => data)
 	.catch((err) => console.log(err));
-
 }
