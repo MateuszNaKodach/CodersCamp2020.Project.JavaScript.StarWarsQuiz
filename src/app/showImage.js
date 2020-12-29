0b3a0db2cd7b40885d9b30a49933ef5b6a7e129e
Pawel Jurecki <jurecki.pawel@gmail.com>
2020-12-29 10:24:34 +0100

    Add unit test for showImage() function

--- src/app/showImage.js
+++ src/app/showImage.js
@@ -1,13 +1,14 @@
-export const showImage = (image)=> {
+export const showImage = (image) => {
 	
 	const app = document.getElementById('swquiz-app')
 	const imageWrapper = document.createElement('div');
 
 	image = atob(image)
-
+imageWrapper.setAttribute('data-testid', 'imageWrapper');
 	imageWrapper.classList.add('swquiz-app-image');
 	imageWrapper.style.backgroundImage = `url(${image})`;
 	app.appendChild(imageWrapper);
+	
 }
 
 
---
+++ test/showImage.spec.js
@@ -0,0 +1,22 @@
+import { screen } from '@testing-library/dom'
+import '@testing-library/jest-dom/extend-expect';
+import {showImage} from './showImage';
+
+test('should add imageWrapper element to DOM and have swquiz-app-image calss', () => {
+
+document.body.innerHTML = `
+<div id="swquiz-app">
+</div>
+`
+
+showImage('c3RhdGljL2Fzc2V0cy9pbWcvbW9kZXMvcGVvcGxlLzQuanBn');
+
+const convertFromBas64 = atob('c3RhdGljL2Fzc2V0cy9pbWcvbW9kZXMvcGVvcGxlLzQuanBn');
+
+expect(screen.getByTestId('imageWrapper')).toBeEmptyDOMElement()
+expect(screen.getByTestId('imageWrapper')).toHaveClass('swquiz-app-image')
+expect(screen.getByTestId('imageWrapper')).toHaveStyle(`
+  background-image: url('${convertFromBas64}');
+`)
+expect(screen.getByTestId('imageWrapper')).toBeInstanceOf(HTMLElement)
+})
