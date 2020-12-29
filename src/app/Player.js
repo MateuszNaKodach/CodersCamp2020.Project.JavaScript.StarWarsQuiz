f2d4e947c735a88312c62c96fd450d5cc823921b
Pawel <pawel.szambelan@flightscope.com.pl>
2020-12-30 00:30:43 +0100

    #24 fix for tests

--- src/app/Player.js
+++ src/app/Player.js
@@ -1,11 +1,13 @@
 export class Player {
   askQuestion(question, onQuestionAsked) {
     if (!onQuestionAsked) return;
-    
+
     onQuestionAsked(question);
   }
 
   answer(chosenAnswer, onQuestionAnswered) {
+    if (!onQuestionAnswered) return;
+
     onQuestionAnswered(chosenAnswer);
   }
 }
--- test/player.spec.js
+++ test/player.spec.js
@@ -2,7 +2,8 @@ import { Player } from '../src/app/Player';
 
 describe("Player's logic", () => {
   const player = new Player();
-  let question;
+  let question;    
+  let chosenAnswer;
 
   it('When player is asked, call onQuestionAsked function with question as parameter', () => {
     const onQuestionAsked = jest.fn();
@@ -13,19 +14,18 @@ describe("Player's logic", () => {
   });
 
   it('When player is asked and onQuesionAsked function is undefined, check if there is no error', () => {
-    // zapytać o drzewo plików po lewej stronie <--------------------
-    let onQuesionAsked;
-
-    expect(() => player.askQuestion(question, onQuesionAsked)).not.toThrow();
-    // expect(player.askQuestion(question, onQuesionAsked)).not.toThrow();
+    expect(() => player.askQuestion(question)).not.toThrow();
   });
 
   it('When player answered, call onQuestionAsnwered function with answer as parameter', () => {
-    let chosenAnswer;
     const onQuestionAnswered = jest.fn();
 
     player.answer(chosenAnswer, onQuestionAnswered);
 
     expect(onQuestionAnswered).toBeCalledWith(chosenAnswer);
+  });
+
+  it('When player answered and onQuestionAnswered function is undefined, check if there is no error', () => {
+    expect(() => player.answer(chosenAnswer)).not.toThrow();
   });
 });
