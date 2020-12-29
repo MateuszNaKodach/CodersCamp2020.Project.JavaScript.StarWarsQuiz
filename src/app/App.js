051182d82d560d49c39ad0bff6108232881ff4d9
PiotrWR <pwrynio@gmail.com>
2020-12-29 19:53:44 +0100

    Add setting of active button

--- src/app/App.js
+++ src/app/App.js
@@ -4,16 +4,16 @@ export const App = ({ options }) => {
   const app = document.getElementById('swquiz-app');
   console.log('Działa App!');
 
-  addNavMenu(app);
+  createNavMenu(app, 0);
 };
 
-function addNavMenu(app) {
+function createNavMenu(app, activeItemNr) {
   app.appendChild(
     NavMenu([
       {
         name: 'people',
         id: 'navMenu__people',
-        isActivated: true,
+        isActivated: activeItemNr == 0,
         fn() {
           console.log('navMenu__people');
         },
@@ -21,7 +21,7 @@ function addNavMenu(app) {
       {
         name: 'vehicles',
         id: 'navMenu__starships',
-        isActivated: false,
+        isActivated: activeItemNr == 1,
         fn() {
           console.log('navMenu__starships');
         },
@@ -29,7 +29,7 @@ function addNavMenu(app) {
       {
         name: 'starships',
         id: 'navMenu__starships',
-        isActivated: false,
+        isActivated: activeItemNr == 2,
         fn() {
           console.log('navMenu__starships');
         },
