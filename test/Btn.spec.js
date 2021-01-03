66568f919043db6b842545958f96fd85c7ec68d0
Tomek <tomasz.dworniczak@hotmail.com>
2021-01-03 20:23:18 +0100

    #13 Button click test
    Add test that checks if button was clicked

--- test/Btn.spec.js
+++ test/Btn.spec.js
@@ -1,28 +1,57 @@
 import { Btn } from '../src/app/components/Btn';
 import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
 
 describe('Function that creates and renders button', () => {
-
-    it('Should create and return button object with passed parameters.', () => {
-        const testButton = Btn({id: 'test-id', btnText: 'Test', class: [], onClickFn: undefined, icon: ''});
-
-        expect(testButton).not.toBeNull();
-        expect(testButton.id).toBe('test-id');
-        expect(testButton.innerText).toBe('Test');
-        expect(testButton).toHaveClass('btn');
-        expect(testButton.onClickFn).toBe(undefined);
-        expect(testButton.icon).toBe(undefined);
-        expect(testButton.innerHTML).toBeNull;
+  it('Should create and return button object with passed parameters.', () => {
+    const testButton = Btn({
+      id: 'test-id',
+      btnText: 'Test',
+      class: [],
+      onClickFn: undefined,
+      icon: '',
     });
 
-    it('Should create and return button with span element in it.', () => {
-        const testButton = Btn({id: 'test-id', btnText: 'Test', class: [], onClickFn: undefined, icon: 'fame'});
+    expect(testButton).not.toBeNull();
+    expect(testButton.id).toBe('test-id');
+    expect(testButton.innerText).toBe('Test');
+    expect(testButton).toHaveClass('btn');
+    expect(testButton.onClickFn).toBe(undefined);
+    expect(testButton.icon).toBe(undefined);
+    expect(testButton.innerHTML).toBeNull;
+  });
 
-        expect(testButton).not.toBeNull();
-        expect(testButton.id).toBe('test-id');
-        expect(testButton.innerText).toBe('Test');
-        expect(testButton).toHaveClass('btn');
-        expect(testButton.onClickFn).toBe(undefined);
-        expect(testButton).toContainHTML('<span class="icon icon--fame"></span>');
+  it('Should create and return button with span element in it.', () => {
+    const testButton = Btn({
+      id: 'test-id',
+      btnText: 'Test',
+      class: [],
+      onClickFn: undefined,
+      icon: 'fame',
     });
-})
+
+    expect(testButton).not.toBeNull();
+    expect(testButton.id).toBe('test-id');
+    expect(testButton.innerText).toBe('Test');
+    expect(testButton).toHaveClass('btn');
+    expect(testButton.onClickFn).toBe(undefined);
+    expect(testButton).toContainHTML('<span class="icon icon--fame"></span>');
+  });
+
+  it('Should test if the button was clicked and onclick function was fired', () => {
+    const testButton = Btn({
+      id: 'test-id',
+      btnText: 'Test',
+      class: [],
+      onClickFn: myMock,
+      icon: 'fame',
+    });
+    const myMock = jest.fn(
+      (testButton.innerText = 'Funkcja onclick zmieniła innerText'),
+    );
+
+    userEvent.click(testButton);
+
+    expect(testButton.innerText).toBe('Funkcja onclick zmieniła innerText');
+  });
+});
