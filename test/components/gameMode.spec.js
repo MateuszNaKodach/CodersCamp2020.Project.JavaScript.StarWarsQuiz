90d61711ebea8aa85aa06440d591227cd05b0284
DomiZet <dominika.zwolenik@gmail.com>
2021-01-07 23:18:22 +0100

    Test fixed

--- test/components/gameMode.spec.js
+++ test/components/gameMode.spec.js
@@ -1,19 +1,20 @@
 import { GameMode } from '../../src/app/components/GameMode';
+import '@testing-library/jest-dom';
 
 describe('GameMode', () => {
   it('Component should be created and displayed', () => {
-    const component = GameMode();
+    const component = GameMode('Wanna play?');
     expect(component).not.toBeNull();
-    expect(component).toHaveClass('gameMode');
+    expect(component.textContent).toMatch(/Wanna play?/);
   });
 
   it('Component should be created with the proper text', () => {
     const component = GameMode('Hello World');
-    expect(component.textContent).toBe('MODE: Hello World');
+    expect(component).toHaveTextContent('MODE: Hello World');
   });
 
   it('Component without any arguments should be created with default text', () => {
     const component = GameMode();
-    expect(component.textContent).toBe('MODE: Who is this character?');
+    expect(component).toHaveTextContent('MODE: Who is this character?');
   });
 });
