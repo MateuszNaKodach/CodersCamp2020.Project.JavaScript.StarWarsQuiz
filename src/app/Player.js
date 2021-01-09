62a838837b600e0ce5422596e5b6131ab3000598
PiotrWR <pwrynio@gmail.com>
2021-01-09 16:17:49 +0100

    Part of work

--- src/app/Player.js
+++ src/app/Player.js
@@ -1,7 +1,8 @@
 export class Player {
+  // przekazujemy pytanie oraz funkcje wywołującą rysowanie pytania
   askQuestion(question, onQuestionAsked) {
     if (!onQuestionAsked) return;
-
+    //
     onQuestionAsked(question);
   }
 
@@ -9,5 +10,15 @@ export class Player {
     if (!onQuestionAnswered) return;
 
     onQuestionAnswered(chosenAnswer);
+    // dupa(aas);
   }
 }
+
+export const computerMind = {};
+
+// To ma być dodane gdzieś indziej
+// const humanPlayer = new Player();
+// const computerPlayer = new Player();
+
+// humanPlayer.askQuestion(question, fn());
+// computerPlayer.askQuestion(question, computerMind);
