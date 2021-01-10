ba87296f77c81b36822d82b8d3cbf1864e8f74b9
PiotrWR <pwrynio@gmail.com>
2021-01-10 17:10:13 +0100

    Create computerMind.js

---
+++ src/app/ComputerMind.js
@@ -0,0 +1,15 @@
+export const computerMind = {
+  question: undefined,
+  setQuestion(question) {
+    computerMind.question = question;
+  },
+  tryToAnswer() {
+    return computerMind.question.answers[randomAnswer(question.answers)];
+  },
+};
+
+const randomAnswer = (questionAnswers) => {
+  const min = 0;
+  const max = questionAnswers.lenght;
+  setTimeout(Math.floor(Math.random() * (max - min)) + min, 300);
+};
---
+++ test/computerMind.spec.js
@@ -0,0 +1,9 @@
+import { Player } from '../src/app/Player';
+import { ComputerMind } from '../src/app/ComputerMind';
+
+describe('Computer Mind', () => {
+  it('When true is true', () => {
+    let flag = true;
+    expect(flag).toBe(true);
+  });
+});
