d4ddb4f69acf121b9454cc05c3bf8ac6f8c3ab9c
Tomek <tomasz.dworniczak@hotmail.com>
2021-01-10 18:04:41 +0100

    #21 Timer function separation of concerns

--- src/app/components/Timer.js
+++ src/app/components/Timer.js
@@ -1,27 +1,30 @@
 export const Timer = (duration) => {
-  let timer = duration,
-    minutes,
-    seconds;
+  let timer = duration;
 
   const timerContainer = document.createElement('div');
   timerContainer.id = 'quiz-timer';
   timerContainer.classList.add('timer');
 
   setInterval(() => {
-    minutes = parseInt(timer / 60, 10);
-    seconds = parseInt(timer % 60, 10);
-
-    minutes = minutes < 10 ? '0' + minutes : minutes;
-    seconds = seconds < 10 ? '0' + seconds : seconds;
-
-    timerContainer.textContent = `Time Left: ${minutes}m ${seconds}s`;
-
-    if (--timer <= 0) {
-      timer = 0;
-    }
+    ({ timer } = countdown(timer, timerContainer));
   }, 1000);
 
   return timerContainer;
 };
 
-function countdown() {}
+function countdown(timer, timerContainer) {
+  let minutes, seconds;
+
+  minutes = parseInt(timer / 60, 10);
+  seconds = parseInt(timer % 60, 10);
+
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  seconds = seconds < 10 ? '0' + seconds : seconds;
+
+  timerContainer.textContent = `Time Left: ${minutes}m ${seconds}s`;
+
+  if (--timer <= 0) {
+    timer = 0;
+  }
+  return { timer };
+}
