44b6fdfd4755fdbdcf8f3e25a7a81be6ffa13a90
Pawel <pawel.szambelan@flightscope.com.pl>
2021-01-11 12:55:51 +0100

    #18 change modal window tests

--- test/modalWindow.spec.js
+++ test/modalWindow.spec.js
@@ -2,38 +2,67 @@ import { ModalWindow } from '../src/app/layouts/ModalWindow';
 import '@testing-library/jest-dom';
 import { screen } from '@testing-library/dom';
 
-describe('Function that creates,show and close modal window', () => {
-  const parent = document.createElement('div');
-  parent.setAttribute('data-testid', 'test-parent');
-  const modalWindow = ModalWindow(parent);
-  const testContent = document.createElement('div');
-  testContent.setAttribute('data-testid', 'test-content');
-  testContent.textContent = 'test content';
+describe('Modal Window', () => {
+  it('when shown, then overlay and the content should be visible', () => {
+    //Given
+    const modalContent = testContent();
+    const modalWindow = testModalWindow((parent) => ModalWindow(parent));
 
-  it('Should display modal window with content', () => {
-    modalWindow.show(testContent);
-    document.body.innerHTML = parent.innerHTML;
+    //When
+    modalWindow.show(modalContent);
 
-    expect(modalWindow).toBeVisible();
-    expect(screen.queryByTestId('test-content')).toHaveTextContent(
-      'test content',
-    );
+    //Then
+    expect(screen.queryByTestId('test-content')).toBeVisible();
+    expect(screen.queryByTestId('test-modal-overlay')).toBeVisible();
+    expect(screen.queryByTestId('test-modal-window')).toBeVisible();
   });
 
-  it('Should hide modal window and delete content', () => {
-    const overlay = modalWindow.firstChild;
-    overlay.setAttribute('data-testid', 'test-overlay');
-    document.body.innerHTML = parent.innerHTML;
+  it('when closed, then content should not be visible', () => {
+    //Given
+    const modalContent = testContent();
+    const modalWindow = shownTestModalWindow((parent) => ModalWindow(parent), {
+      content: modalContent,
+    });
 
-    // here modalWindow contain content
-    console.log('modalWindow', modalWindow.innerHTML);
-
+    //When
     modalWindow.close();
-    // here modalWindow still contain content...why?
-    console.log('modalWindow after close method', modalWindow.innerHTML);
 
-    expect(modalWindow).not.toBeVisible();
-    // in that case this test is wrong ... -.-
-    expect(screen.queryByTestId('test-overlay')).toBeEmptyDOMElement();
+    //Then
+    expect(screen.queryByTestId('test-content')).not.toBeInTheDocument();
+    expect(screen.queryByTestId('test-modal-overlay')).not.toBeVisible();
+    expect(screen.queryByTestId('test-modal-window')).not.toBeVisible();
+  });
+
+  it('when created, then should not be visible', () => {
+    //When
+    const modalWindow = testModalWindow((parent) => ModalWindow(parent));
+
+    //Then
+    expect(screen.queryByTestId('test-modal-overlay')).not.toBeVisible();
+    expect(screen.queryByTestId('test-modal-window')).not.toBeVisible();
   });
 });
+
+function testModalWindow(modalFn) {
+  document.body.innerHTML = `<div id="component-container"></div>`;
+  const componentContainer = document.getElementById('component-container');
+  const modalWindow = modalFn(componentContainer);
+  modalWindow.setAttribute('data-testid', 'test-modal-window');
+  modalWindow.style.display = 'none';
+  const modalOverlay = modalWindow.querySelector('#overlay');
+  modalOverlay.setAttribute('data-testid', 'test-modal-overlay');
+  return modalWindow;
+}
+
+function shownTestModalWindow(modalFn, { content }) {
+  const testModal = testModalWindow(modalFn);
+  testModal.show(content);
+  return testModal;
+}
+
+function testContent() {
+  const testContent = document.createElement('div');
+  testContent.setAttribute('data-testid', 'test-content');
+  testContent.textContent = 'test content text';
+  return testContent;
+}
