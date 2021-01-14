faeb3afcc0f827109b0ac4e88b71845f86bf3a23
Tomek <tomasz.dworniczak@hotmail.com>
2021-01-14 18:37:53 +0100

    #10 Refactor code and styles

--- src/app/components/ModeRanking.js
+++ src/app/components/ModeRanking.js
@@ -2,47 +2,45 @@ export const ModeRanking = (topScores) => {
   const modeRankingObj = document.createElement('div');
   modeRankingObj.classList.add('modeRanking');
 
-  if (typeof topScores !== 'undefined' && topScores.length > 0) {
-    const headerContainer = document.createElement('div');
-    headerContainer.classList.add('modeRanking__headerContainer');
+  const headerContainer = document.createElement('div');
+  headerContainer.classList.add('modeRanking__headerContainer');
 
-    const rankingIcon = document.createElement('span');
-    rankingIcon.classList.add('modeRanking__icon');
-    headerContainer.appendChild(rankingIcon);
+  const rankingIcon = document.createElement('span');
+  rankingIcon.classList.add('modeRanking__icon');
+  headerContainer.appendChild(rankingIcon);
 
-    const rankingHeader = document.createElement('h2');
-    rankingHeader.classList.add('modeRanking__header');
-    rankingHeader.innerText = 'Mode Ranking';
-    headerContainer.appendChild(rankingHeader);
+  const rankingHeader = document.createElement('h2');
+  rankingHeader.classList.add('modeRanking__header');
+  rankingHeader.innerText = 'Mode Ranking';
+  headerContainer.appendChild(rankingHeader);
 
-    modeRankingObj.appendChild(headerContainer);
+  modeRankingObj.appendChild(headerContainer);
 
-    const rankingContainer = document.createElement('div');
-    rankingContainer.classList.add('modeRanking__rankingContainer');
+  const rankingContainer = document.createElement('div');
+  rankingContainer.classList.add('modeRanking__rankingContainer');
 
-    const placesList = ['1st', '2nd', '3rd'];
+  const placesList = ['1st', '2nd', '3rd'];
 
-    const titleRow = document.createElement('header');
-    titleRow.classList.add(
-      'modeRanking__rankingRow',
-      'modeRanking__rankingRow--title',
+  const titleRow = document.createElement('header');
+  titleRow.classList.add(
+    'modeRanking__rankingRow',
+    'modeRanking__rankingRow--title',
+  );
+
+  const headerElementTitles = ['Place', 'Player', 'Answered'];
+
+  for (let i = 0; i < headerElementTitles.length; i++) {
+    rankingContainer.appendChild(titleRow);
+    const headerElement = document.createElement('div');
+    headerElement.classList.add(
+      'modeRanking__headerItem',
+      `modeRanking__headerItem--${headerElementTitles[i].toLowerCase()}`,
     );
+    headerElement.textContent = headerElementTitles[i];
+    titleRow.appendChild(headerElement);
+  }
 
-    const headerElementTitles = ['Place', 'Player', 'Answered'];
-
-    for (let i = 0; i < headerElementTitles.length; i++) {
-      rankingContainer.appendChild(titleRow);
-      const headerElement = document.createElement('div');
-      headerElement.classList.add(
-        'modeRanking__headerItem',
-        `modeRanking__headerItem--${headerElementTitles[i].toLowerCase()}`,
-      );
-      headerElement.textContent = headerElementTitles[i];
-      titleRow.appendChild(headerElement);
-    }
-
-    // * koniec pętli
-
+  if (typeof topScores !== 'undefined' && topScores.length > 0) {
     for (let i = 0; i < topScores.length; i++) {
       const playerRow = document.createElement('div');
       playerRow.classList.add('modeRanking__rankingRow');
@@ -65,17 +63,15 @@ export const ModeRanking = (topScores) => {
 
       rankingContainer.appendChild(playerRow);
     }
-
-    // * koniec pętli
-
-    modeRankingObj.appendChild(rankingContainer);
   } else {
     const emptyRankingElem = document.createElement('p');
-    emptyRankingElem.classList.add('modeRanking__text');
+    emptyRankingElem.classList.add('modeRanking__emptyLeaderboardText');
     emptyRankingElem.setAttribute('data-testid', '');
     emptyRankingElem.textContent = 'Leaderboard is empty...';
-    modeRankingObj.appendChild(emptyRankingElem);
+    rankingContainer.appendChild(emptyRankingElem);
   }
+
+  modeRankingObj.appendChild(rankingContainer);
 
   return modeRankingObj;
 };
