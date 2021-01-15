35fbec0f3ec646ae589f3f07b254cab4877e3c08
Tomek <tomasz.dworniczak@hotmail.com>
2021-01-15 12:37:04 +0100

    #21 refactor

---
+++ src/app/components/TextTimer.js
@@ -0,0 +1,32 @@
+export const TextTimer = (duration, timeSource) => {
+  let timer = duration;
+
+  const timerContainer = document.createElement('div');
+  timerContainer.id = 'quiz-timer';
+  timerContainer.classList.add('timer');
+
+  setInterval(() => {
+    timer = countdown(timer, timerContainer);
+  }, 1000);
+
+  return timerContainer;
+};
+
+export function countdown(timer, htmlElem) {
+  let minutes, seconds;
+
+  minutes = parseInt(timer / 60, 10);
+  seconds = parseInt(timer % 60, 10);
+
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  seconds = seconds < 10 ? '0' + seconds : seconds;
+
+  htmlElem.textContent = `Time Left: ${minutes}m ${seconds}s`;
+
+  if (--timer <= 0) {
+    timer = 0;
+  }
+  return timer;
+}
+
+class IntervalTimer {}
