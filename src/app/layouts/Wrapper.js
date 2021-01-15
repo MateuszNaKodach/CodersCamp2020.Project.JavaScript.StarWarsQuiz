d4410330eb8d9fed00d5d0954a6adbc8a872e4dd
PiotrWR <pwrynio@gmail.com>
2021-01-15 14:50:21 +0100

    Add TODO in Wrapper ( @PiotrWR & @tomdworniczak )

--- src/app/layouts/Wrapper.js
+++ src/app/layouts/Wrapper.js
@@ -5,3 +5,33 @@ export const Wrapper = () => {
 
   return wrapper;
 };
+
+// TODO: funkcja wypełniająca poczatkowy stan wrappera
+{
+  // TODO: Ustaw logo
+  // TODO: Ustaw menu
+  // TODO: ustaw photo
+  // TODO: ustaw mainContainer
+  // TODO: Kliknięcie w nav menu wywołuje funkcję (cleanAndFill() lub cleanAndFillMainContainer() - nazwa do wyboru):
+  // TODO: -- niszczy dzieci mainContainer
+  // TODO: -- niszczy wywpłuje wypelnienie mainContainer
+  // TODO: -- oparty na "switch"?
+
+  // TODO: Wypełnij main mainContainer:
+  {
+    // TODO:  funkcja wywołująca ustawine początkowe
+    // TODO:  -- Mode Title
+    // TODO:  -- Container (Rules lub ranking)
+    // TODO:  -- Dwa przyciski
+    // TODO:  funkcja podmieniająca  btn na ranking
+    // TODO:  -- small container
+    // TODO:  -- btns
+  }
+  // TODO: Kliknięcie przycisku PLAY wywoła metodę cleanAndFill();
+  // TODO:
+  // TODO:
+  // TODO:
+  // TODO:
+  // TODO:
+  // TODO:
+}
