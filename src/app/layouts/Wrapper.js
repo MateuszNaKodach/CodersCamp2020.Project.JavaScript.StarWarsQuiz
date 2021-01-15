b73be31cf00693f851536eb57034e10274178c82
PiotrWR <pwrynio@gmail.com>
2021-01-15 15:55:22 +0100

    Add setStartView function ( @PiotrWR & @tomdworniczak )

--- src/app/layouts/Wrapper.js
+++ src/app/layouts/Wrapper.js
@@ -1,19 +1,75 @@
+import { render } from '../rendering';
+import { Logo } from './Logo';
+import { NavMenu } from './NavMenu';
+import { MainContainer } from './MainContainer';
+import { QuestionImage } from './QuestionImage';
+
 export const Wrapper = () => {
   const wrapper = document.createElement('div');
   wrapper.classList.add('wrapper');
   wrapper.id = 'wrapper';
 
+  setStartView(wrapper);
+
   return wrapper;
 };
 
-// TODO: funkcja wypełniająca poczatkowy stan wrappera
+function setStartView(parent) {
+  const logo = render({
+    component: Logo(),
+    inside: parent,
+    withClasses: 'wrapper__logo',
+  });
+  const questionImage = render({
+    component: QuestionImage(
+      'c3RhdGljL2Fzc2V0cy9pbWcvbW9kZXMvcGVvcGxlLzQuanBn',
+    ),
+    inside: parent,
+    withClasses: 'wrapper__mainImg',
+  });
+  const mainContainer = render({
+    component: MainContainer(),
+    inside: parent,
+    withClasses: 'wrapper__mainContainer',
+  });
 
-// TODO: Ustaw logo
-// TODO: Ustaw menu
-// TODO: ustaw photo
-// TODO: ustaw mainContainer
+  renderNavMenu(parent);
+}
 
-//////
+function renderNavMenu(parent, activeItemNr = 0, previousState = undefined) {
+  if (parent && previousState) {
+    parent.removeChild(previousState);
+  }
+
+  const component = NavMenu([
+    {
+      name: 'people',
+      id: 'navMenu__people',
+      isActivated: activeItemNr == 0,
+      onClickFn() {
+        renderNavMenu(parent, 0, component);
+      },
+    },
+    {
+      name: 'vehicles',
+      id: 'navMenu__vehicles',
+      isActivated: activeItemNr == 1,
+      onClickFn() {
+        renderNavMenu(parent, 1, component);
+      },
+    },
+    {
+      name: 'starships',
+      id: 'navMenu__starships',
+      isActivated: activeItemNr == 2,
+      onClickFn() {
+        renderNavMenu(parent, 2, component);
+      },
+    },
+  ]);
+
+  return render({ component, inside: parent, withClasses: 'wrapper__nav' });
+}
 
 // TODO: Kliknięcie w nav menu wywołuje funkcję (cleanAndFill() lub cleanAndFillMainContainer() - nazwa do wyboru):
 // TODO: -- niszczy dzieci mainContainer
