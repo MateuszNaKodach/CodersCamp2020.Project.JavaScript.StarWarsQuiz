dcbac85ad1d4ecedf6cd124cce6893b350f9ea57
Piotr Witold Rynio <41823689+PiotrWR@users.noreply.github.com>
2021-01-18 00:23:33 +0100

    Quiz view before merge (#86)
    * Unblock Animation
    
    * Added Template Wirtual GameManager Machine!
    
    * #22 Add mode titlie generation
    
    * Working UI without IMG
    
    * Add Change Img Function
    
    * #22 Cleaning files
    
    * Update GameQuizView
    
    * Update names
    
    * Add small tests
    
    * After CR
    
    * Update Constructor
    
    * Finish CReview
    
    * Fix of GameQuizView

--- src/app/views/GameQuizView.js
+++ src/app/views/GameQuizView.js
@@ -3,6 +3,7 @@ import { QuestionAnswers as questionAnswersButtonsBoxComponent } from '../compon
 import { arrayIdNames } from '../settings';
 import { ModalWindow } from '../layouts/ModalWindow';
 import { ModalWindowContent } from '../layouts/ModalWindowContent';
+import { Gameplay } from './../Gameplay';
 
 export class GameQuizView {
   constructor(
@@ -25,7 +26,7 @@ export class GameQuizView {
     this._renderWaitingTitleComponent();
 
     // ! TUTAJ ODDAJEMY GŁOS Maszynie GameManager !
-    this.gameManager = new TemplateClass(
+    this.gameManager = new Gameplay(
       (questionObjectFromGameManager) =>
         this._setQuestionFromGameManager(questionObjectFromGameManager),
 
@@ -42,7 +43,6 @@ export class GameQuizView {
   }
 
   // ******************************************************
-
   _setEndOfGame(
     player1answersArray = [
       { id: 1, isCorrect: true },
@@ -96,16 +96,14 @@ export class GameQuizView {
     console.log('Strona powinna przeładować się automatycznie!');
   }
 
+  _setNewRecordDatabase() {}
+
   _setUpdatedTime(time) {
     console.log(`Time: ${time} ms`);
     console.log('DODAJ tu TEXT TIMER');
     // TODO: TEXT TIMER W TYM MIEJSCU
   }
 
-  _setEndOfGame() {
-    console.log('Gra zakończyła się!');
-    console.log('Strona powinna przeładować się automatycznie!');
-  }
   // ******************************************************
   _renderWaitingTitleComponent() {
     this.settings.renderComponentsFromComponentsArrayCallbackFunction([
@@ -207,109 +205,4 @@ function modifiedGameModeComponent(
   }
 
   return questionContainer;
-}
-
-// ******************************************************
-// ******************************************************
-// ******************************************************
-// ! Przykład dla programistów --->>>
-// ! WIRTUALNA Game MASZYNA!
-// ! WIRTUALNY TEST!
-
-class TemplateClass {
-  constructor(
-    setQuestionFromGameManagerCallBackFunction,
-    setEndOfGameCallbackFunction,
-  ) {
-    this.callbackFunction_setQuestionFromGameManager = setQuestionFromGameManagerCallBackFunction;
-    this.callbackFunction_setEndOfGame = setEndOfGameCallbackFunction;
-    this.templateGeneratorClass;
-    this._templateMethods_1();
-  }
-
-  _templateMethods_1() {
-    this.templateGeneratorClass = new TemplateGeneratorClass();
-    this._setQuestionInUI();
-  }
-
-  _setQuestionInUI() {
-    this.templateGeneratorClass.getGenereatedQuestion((returnedObj) => {
-      if (returnedObj)
-        this.callbackFunction_setQuestionFromGameManager(returnedObj);
-      else {
-        this._templateEndOfGame();
-      }
-    });
-  }
-  setRankingSaving() {
-    console.log('Zapisano ranking');
-  }
-  setPlayerAnswer() {
-    this._saveData();
-    this._setQuestionInUI();
-  }
-
-  _saveData() {}
-
-  _templateEndOfGame() {
-    console.log('KONIEC GRY!');
-    this.callbackFunction_setEndOfGame();
-    let timeToWindowReload = 5000;
-    setInterval(() => {
-      console.log(
-        `Strona zostanie przeładowana za: ${timeToWindowReload / 1000}`,
-      );
-      timeToWindowReload -= 1000;
-    }, 1000);
-    // setTimeout(() => window.location.reload(), timeToWindowReload + 1000);
-  }
-}
-
-// ******************************************************
-
-class TemplateGeneratorClass {
-  constructor() {
-    this.itemNumber = 0;
-    this.internetServer = [
-      {
-        answers: ['Luke Skywalker', 'R2-D2', 'Chewbacca', 'Boba Fett'],
-        image: { mode: 'people', rightAnswer: 1 },
-        rightAnswer: 'Luke Skywalker',
-      },
-      {
-        answers: ['Brzuszek', 'R2-D2', 'Kot filemon', 'JSON'],
-        image: { mode: 'people', rightAnswer: 2 },
-        rightAnswer: 'R2-D2',
-      },
-      {
-        answers: ['Brzuszek', 'Kot filemon', 'JSON', 'C-3PO'],
-        image: { mode: 'people', rightAnswer: 3 },
-        rightAnswer: 'C-3PO',
-      },
-    ];
-  }
-
-  getGenereatedQuestion(callbackFunctionFromTampleClass) {
-    const downloadingTime = 500;
-    console.log(`----------------------------------------`);
-    console.log(`UWAGA!`);
-    console.log(`Trwa pobieranie pytania...`);
-    console.log(
-      `Przybliżony czas oczekiwania na pytanie to ${downloadingTime} sec`,
-    );
-    console.log(`(Może zostać naliczona opłata za transmisje danych )`);
-    console.log(`----------------------------------------`);
-    setTimeout(() => {
-      console.log(`----------------------------------------`);
-      console.log(`UWAGA! Pobrano pytanie z internetu! `);
-      console.log(`Czas pobierania wynosi ${downloadingTime} sec`);
-      console.log(
-        `-->>>>> UWAGA! Naliczono %c${(Math.random() * 4).toFixed(2)} zł`,
-        'background: yellow;  font-weight: bold; ',
-        `złotego opłaty za transmisje!`,
-      );
-      console.log(`----------------------------------------`);
-      callbackFunctionFromTampleClass(this.internetServer[this.itemNumber++]);
-    }, downloadingTime);
-  }
 }
