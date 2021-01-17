6d1f4a96a688b49e252916c7f182620cb97697b5
DomiZet <dominika.zwolenik@gmail.com>
2021-01-17 21:32:46 +0100

    Apply BEM convenction into styles

--- src/app/components/Sword.js
+++ src/app/components/Sword.js
@@ -3,28 +3,28 @@ export const Sword = () => {
   component.classList.add('sword');
 
   const swordWrap = document.createElement('div');
-  swordWrap.classList.add('sword-wrap');
+  swordWrap.classList.add('sword__wrap');
   component.appendChild(swordWrap);
 
   const swordHilt = document.createElement('div');
-  swordHilt.classList.add('sword-hilt');
+  swordHilt.classList.add('sword__wrap__hilt');
   swordWrap.appendChild(swordHilt);
 
   const swordHiltImage = document.createElement('img');
-  swordHiltImage.classList.add('sword-hilt-image');
+  swordHiltImage.classList.add('sword__wrap__hilt--image');
   swordHiltImage.setAttribute('src', 'static/assets/ui/LightsaberHandle.png');
   swordHilt.appendChild(swordHiltImage);
 
   const powerTrack = document.createElement('div');
-  powerTrack.classList.add('power-track');
+  powerTrack.classList.add('sword__wrap__power');
   swordWrap.appendChild(powerTrack);
 
   const powerTrackLight = document.createElement('div');
-  powerTrackLight.classList.add('power-track-light');
+  powerTrackLight.classList.add('sword__wrap__power--light');
   powerTrack.appendChild(powerTrackLight);
 
   const startCountingDown = () => {
-    powerTrackLight.classList.add('swordAnimation');
+    powerTrackLight.classList.add('sword__wrap__power--animation');
   };
 
   const button = document.querySelector('.mainContainer__playTheGameButton');
